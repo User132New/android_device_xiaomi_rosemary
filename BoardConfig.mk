e96c64c10b25d65aa4050059ea3b3bfc172af19e
dreAd <dreadcreator5@gmail.com>
2023-10-23 06:16:39 +0000

    rosemary:SELinux Permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,6 +158,9 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
+# SELinux Permissive
+BOARD_KERNEL_CMDLINE     += androidboot.selinux=permissive
+
 # SPL
 VENDOR_SECURITY_PATCH := 2022-03-01
 

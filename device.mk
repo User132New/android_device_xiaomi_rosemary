9dee77c8fd399daf68e6e29a0bd93355c3d23a3c
Beru Krugermeier <windowz414@gnuweeb.org>
2023-11-12 14:04:30 +0100

    rosemary: Build vendor variant of `android.frameworks.sensorservice`
    Signed-off-by: Beru Krugermeier <windowz414@gnuweeb.org>

--- device.mk
+++ device.mk
@@ -436,6 +436,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice.vendor \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.1-service.multihal \
     android.hardware.sensors@1.0.vendor \

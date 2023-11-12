8220573e85132d2f8ae5aca18afb1fc7cc0a8f18
bengris32 <bengris32@protonmail.ch>
2023-11-12 13:45:37 +0100

    rosemary: Switch to AOSP hostapd service
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I1f634de16e59f194784ac5f2863c1a00de321876
    Signed-off-by: Beru Krugermeier <windowz414@gnuweeb.org>

--- device.mk
+++ device.mk
@@ -480,13 +480,9 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi.supplicant@1.0.vendor \
-    android.hardware.wifi.supplicant@1.1.vendor \
-    android.hardware.wifi.supplicant@1.2.vendor \
+    hostapd \
     android.hardware.wifi.supplicant@1.3.vendor \
-    android.hardware.wifi.hostapd@1.0.vendor \
-    android.hardware.wifi.hostapd@1.1.vendor \
-    android.hardware.wifi.hostapd@1.2.vendor
+    android.hardware.wifi@1.3.vendor
 
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service-lazy.rosemary
